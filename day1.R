a60de8ef5ce2c299d81d45a6fc25a56f833c03da
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2021-12-01 16:27:55 +1030

    Using scan for vector

--- day1.R
+++ day1.R
@@ -1,29 +1,29 @@
 # 2021 Advent of Code
 # Problem 1
 
-input <- read.table("input1.txt")
+input <- scan("input1.txt")
 
 # First solution
 increasing <- 0
-for(i in 2:nrow(input)) {
-    if(input$V1[i] > input$V1[i-1]) {
+for(i in 2:length(input)) {
+    if(input[i] > input[i-1]) {
         increasing <- increasing + 1
     }
 }
 
 # Vectorised solution
-sum(c(input$V1, NA) > c(NA, input$V1), na.rm = T)
+sum(c(input, NA) > c(NA, input), na.rm = T)
 
 # First solution
 increasing <- 0
-for(i in 4:nrow(input)) {
-    s1 <- sum(input$V1[(i-1):(i-3)])
-    s2 <- sum(input$V1[i:(i-2)])
+for(i in 4:length(input)) {
+    s1 <- sum(input[(i-1):(i-3)])
+    s2 <- sum(input[i:(i-2)])
     if(s2 > s1) {
         increasing <- increasing + 1
     }
 }
 
 # Vectorised solution
-sums <- rowSums(data.frame(c(NA, NA, input$V1), c(NA, input$V1, NA), c(input$V1, NA, NA)))
+sums <- rowSums(data.frame(c(NA, NA, input), c(NA, input, NA), c(input, NA, NA)))
 sum(c(sums, NA) > c(NA, sums), na.rm = T)
